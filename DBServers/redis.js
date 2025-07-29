df967daadd29aaf68516a2d419413c24fe256904
ezra-tarab <ezra5385@gmail.com>
2025-07-29 20:17:26 +0300

    changed for docker

--- DBServers/redis.js
+++ DBServers/redis.js
@@ -8,7 +8,13 @@ async function getRedisClient() {
 
   if (!isConnecting) {
     isConnecting = true;
-    client = redis.createClient();
+
+    client = redis.createClient({
+      socket: {
+        host: process.env.REDIS_HOST || "127.0.0.1",
+        port: process.env.REDIS_PORT ? Number(process.env.REDIS_PORT) : 6379,
+      },
+    });
 
     client.on("error", (err) => {
       console.error("❌ Redis client error:", err);
